02eaa17c668d5ed146c59f5a34a16b74146c104a
Joe Robinson <joe.robinson@ismconnect.com>
2019-07-17 16:47:35 -0400

    Added function transitively_merge_clusters() to cluster in bottom-up fashion given distance matrix D and NN lut for all N samples making up NxN items of D.
    BUG FIX: flipped scoring-- was summing instances of shared neighbors, when should have been summing 'missed' shared neighbors, i.e., lower is better, so sum up with 0 when neighbor is shared, else 1 (normalization at bottom of calculate_rank_order_distance()).
    
    sweeping values for eps in __main__()

--- rankorder.py
+++ rankorder.py
@@ -23,74 +23,118 @@ import time
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
+
 def build_nn_lists(feature_file, k=20, algorithm='kd_tree'):
     """Build knn lists for N samples. Return Nxk array with N being each sample index and k being knn indices"""
 
-    print('##### Build Tree #####')
-
     feature_dict = pd.read_pickle(feature_file)
 
-
     #  Compute top-k NN for each face encoding
-    nbrs = NearestNeighbors(n_neighbors=k+1, algorithm=algorithm).fit(feature_dict['X'])
+    nbrs = NearestNeighbors(n_neighbors=k + 1, algorithm=algorithm).fit(feature_dict['X'])
     distances, indices = nbrs.kneighbors(feature_dict['X'])
 
     return indices
 
 
 def calculate_rank_order_distance(nn_ids):
-
-    nsamples, k = nn_ids.shape
+    nsamples = len(nn_ids)
     # rank order distances between samples and k-NN
-    D = np.full((nsamples,nsamples), np.inf)
+    D = np.full((nsamples, nsamples), np.inf)
 
     # track samples that have been compared
     touched = np.zeros(nsamples).astype(np.float)
-    for x in tqdm(range(nsamples)):
+    for a_id, key in tqdm(enumerate(nn_ids.keys())):
         # for each sample
 
         # current face encoding (i.e., face a)
-        a_id = x                               # id of sample face a
-        a_nnlist = nn_ids[a_id,:] - 1            # NN list for face a
+        a_nnlist = nn_ids[key]  # NN list for face a
 
-        for y, b_id in enumerate(a_nnlist):
-            # for each of its k-NN, id of sample face b
-            # kth NN of face a (i.e., face b)
+        bdict = {a: nn_ids[a] for a in a_nnlist}
 
-            if not np.isinf(D[b_id,a_id]):
-                # if distance between pairs was calculated (i.e., if face_b has been as face_a)
+        for rank_b, b_id in enumerate(bdict.keys()):
+            # for each of its k-NN, id of sample face b is also its rank on NN of face a.
+
+            if not np.isinf(D[rank_b, a_id]):
+                # if already calculated (i.e., if face_b has been as face_a)
                 continue
 
-            b_nnlist = nn_ids[b_id,:] - 1        # NN list for face a
-
+            b_nnlist = nn_ids[rank_b]  # NN list for face a
 
             # if face a is in face b's NN list, then determine its rank;
             rank_a = np.where(b_nnlist == a_id)[0]
+            if len(rank_a) == 0:
+                # min(O_a, k), where O_a is rank of NN list, thus, set as k if match is not in top k
+                rank_a = k
 
-            if len(rank_a)==0:
-                rank_a = [k - 1]
-                # if rank_a < k and b_id has been touched, then distance has
-                # already been computed between faces (with a-b flipped)
-            elif touched[b_id]==1:
-                print("?")
-            #  determine rank of face b in face a's NN list (just y)
-            rank_b = y
+            elif touched[b_id] == 1:
+                print("Recalculated distances for face pairs in reverse order (i.e., b-a, flipped)... shouldn't be'")
+
+            rank_a = int(rank_a)
+
+            denom = rank_a + 1 if rank_a < rank_b else rank_b + 1
 
             #  assymmetric rank order distance (0 for every shared NN; else, 1);
-            d_ba = sum([np.any(a == b_nnlist) for a in a_nnlist[:rank_b]])
-            d_ab = sum([np.any(b == a_nnlist) for b in b_nnlist[rank_a]])
+            d_ba = sum([not np.any(a == b_nnlist) for a in a_nnlist[:rank_b]])
+            d_ab = sum([not np.any(b == a_nnlist) for b in b_nnlist[:rank_a]])
 
-            #  rank-order distance
-            denom = rank_a[0] if rank_a[0] < rank_b else rank_b
-            # if denom==0:
-            #     print()
-            D[a_id,b_id] = (d_ab + d_ba)/denom if denom > 0 else 0
-            D[b_id,a_id] = D[a_id,b_id]
-            if np.isnan(D[b_id,a_id]):
-                print()
+            D[a_id, b_id] = (d_ab + d_ba) / denom if denom > 0 else np.nan
+            D[b_id, a_id] = D[a_id, b_id]
+            if np.isnan(D[b_id, a_id]):
+                print('NAN. Likely an attempt to divide by zero when normalizing above.')
         #  mark current face for being compared to its entire NN list
         touched[a_id] = 1
     return D
+
+
+def transitively_merge_clusters(D, nn_dict, Eps=1.6, C=None):
+    ##  Threshold rank-order distance matrix.
+    #  Transitively step through matrix, merging each pairs w distances below
+    #  threshold Eps into same cluster.
+    #
+    #  Provided constraint matrix C, 'must-' and
+    #
+    #  @param D     Rank order distance matrix
+    #  @param Eps   Rank order distance threshold (i.e., epsilon) [default 1.6]
+    #  @param C     Constraint matrix [default zeros(size(D))]
+    #
+    #  @return cluster_tags    Cluster ID assignments
+    #
+    #  @author Joseph P. Robinson
+    #  @date 2019 July 17
+    #
+    #  SEE CALCULATE_RANK_ORDER_DISTANCE()
+    #
+
+    if C is None:
+        C = np.zeros(D.shape)
+    nsamples = D.shape[0]
+
+    #  cluster ids -- initialize all samples in own cluster
+    cluster_tags = np.arange(nsamples)
+
+    #  Determine distances that follow below threshold AND are not constrained
+    #  according to matrix C as 'cannot link' (i.e., c(i,j) ~= -1) OR matrix C
+    #  forces a merge via a reference to 'must link' (i.e., c(i,j) == 1)
+    to_merge = np.zeros_like(D)
+    to_merge[np.eye(len(to_merge)) == 1] = 1
+    to_merge[np.where(D < Eps)] = 1
+
+    # apply constraints
+    to_merge[np.where(C == 1)] = 1
+    to_merge[np.where(C == -1)] = 0
+
+    for x in range(nsamples - 1):
+        #  for each element of upper triangle of symmetric distance matrix
+        #      distances = D(x,(x+1):end);
+        #      to_merge = distances < Eps;
+        c_to_merge = to_merge[x, x:] == 1
+        # print(c_to_merge)
+        c_tag = np.min(cluster_tags[x:][c_to_merge])
+        # c_tag = np.min([cluster_tags[x], cluster_tags[x+1:] & c_to_merge)]);
+        cluster_tags[x:][c_to_merge] = c_tag
+
+    return cluster_tags
+
 
 def check_files_and_conditions(build_knn, calculate_dmatrix, file_knn_matrix, file_d_matrix, file_feature):
     if is_file(file_feature):
@@ -102,9 +146,10 @@ def check_files_and_conditions(build_knn, calculate_dmatrix, file_knn_matrix, fi
     print("Check features files and/or KNN list or D-Matrix exist.")
     return False
 
+
 if __name__ == '__main__':
     build_knn = False
-    calculate_dmatrix = False
+    calculate_dmatrix = True
     k = 20
     file_knn_matrix = 'kd_tree_20.pkl'
     file_d_matrix = 'dmatrix_20.pkl'
@@ -116,9 +161,16 @@ if __name__ == '__main__':
 
     ids_mat = pd.read_pickle(file_knn_matrix) if (not build_knn and is_file(file_knn_matrix)) else \
         build_nn_lists(file_feature)
-
-    dmatrix = pd.read_pickle(file_d_matrix) if (not calculate_dmatrix and is_file(file_d_matrix)) else\
-        calculate_rank_order_distance(ids_mat)
+    nn_lut = {id[0]: id[1:] for id in ids_mat}
+    dmatrix = pd.read_pickle(file_d_matrix) if (not calculate_dmatrix and is_file(file_d_matrix)) else \
+        calculate_rank_order_distance(nn_lut)
 
     print("Took {}".format(time.time() - start))
-    print()
+
+    eps_arr = np.arange(0.1, 3, 0.1)
+    eps_arr = np.concatenate([list(eps_arr), list(np.arange(3, 20, 1))])
+    results_lut = {}
+    for eps in tqdm(eps_arr):
+        results_lut[eps] = transitively_merge_clusters(dmatrix, nn_lut, eps)
+
+    pd.to_pickle(results_lut, 'results_lut2.pkl')
